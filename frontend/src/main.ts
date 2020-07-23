3f94c8083163880b77b11fe7b17e06a48aaf5c38
gregorybarros <gregory.barros@hotmail.com>
2020-07-23 17:21:37 -0300

    Initial commit

---
+++ frontend/src/main.ts
@@ -0,0 +1,15 @@
+// Porta de entrada, importa algumas coisas... e 
+// Carrega o principal (AppModule) o modulo padrao.
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
